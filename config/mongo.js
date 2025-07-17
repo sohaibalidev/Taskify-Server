9a4f295f99742feef4c2365e757b87f96d2c3842
sohaibalidev <msohaibaliofficial@gmail.com>
2025-07-17 10:23:49 +0500

    update

--- config/mongo.js
+++ config/mongo.js
@@ -1,19 +1,16 @@
 const { MongoClient } = require('mongodb');
 const config = require('../config/config')
 
-const MONGO_URI = config.MONGO_URI;
-const DB_NAME = config.DB_NAME;
-
 let db = null;
 let client = null;
 
 async function connectDB() {
     if (db) return db;
 
-    client = new MongoClient(MONGO_URI);
+    client = new MongoClient(config.MONGO_URI);
     try {
         await client.connect();
-        db = client.db(DB_NAME);
+        db = client.db(`${config.DB_NAME}`);
 
         return db;
     } catch (err) {
